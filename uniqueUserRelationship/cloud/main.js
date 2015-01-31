3f25e8c71ca58bf829b10368c1215060d0f9a838
Troy Ling <troyling14@gmail.com>
2015-01-31 14:17:17 -0500

    bug fix

--- uniqueUserRelationship/cloud/main.js
+++ uniqueUserRelationship/cloud/main.js
@@ -5,7 +5,7 @@ var userRelationship = Parse.Object.extend("UserRelationship");
 
 // check if the relationship has been created
 Parse.Cloud.beforeSave("UserRelationship", function(request, response) {
-	if (request.object.get("followedUser") && request.object.get("following")) {
+	if (request.object.get("followedUser") && request.object.get("follower")) {
 		var query = new Parse.Query(userRelationship);
 		query.equalTo("followedUser", request.object.get("followedUser"));
 		query.equalTo("follower", request.object.get("follower"));

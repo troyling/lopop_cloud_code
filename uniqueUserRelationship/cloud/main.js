3998d2fa8a3c296cdf0c4061c16fa7ed807add96
Troy Ling <troyling14@gmail.com>
2015-01-31 13:54:26 -0500

    add cloud code to validate uniqueness of the relationship

--- uniqueUserRelationship/cloud/main.js
+++ uniqueUserRelationship/cloud/main.js
@@ -1,6 +1,26 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var userRelationship = Parse.Object.extend("UserRelationship");
+
+// check if the relationship has been created
+Parse.Cloud.beforeSave("UserRelationship", function(request, response) {
+	if (request.object.get("followedUser") && request.object.get("following")) {
+		var query = new Parse.Query(userRelationship);
+		query.equalTo("followedUser", request.object.get("followedUser"));
+		query.equalTo("follower", request.object.get("follower"));
+		query.count({
+			success: function(number) {
+				// relationship existed
+				if (number != 0) {
+					response.error("You have already been following the user.");
+				} else {
+					response.success();
+				}
+			},
+			error: function(error) {
+				response.error("Something went wrong with validation.");
+			}
+		});
+	}
 });

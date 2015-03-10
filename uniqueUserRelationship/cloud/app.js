170e6c58ed562f08b8e17fa1886ddcf02a5dfe6b
Hongbo Fang <Hongbo@HongbodeMacBook-Pro.local>
2015-03-10 16:08:51 -0400

    start testing bootstrap
    created pop page

--- uniqueUserRelationship/cloud/app.js
+++ uniqueUserRelationship/cloud/app.js
@@ -9,15 +9,59 @@ app.use(express.bodyParser());    // Middleware for reading request body
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
-app.get('/hello', function(req, res) {
-	res.render('mytest', {customized_name: 'lopop page', username: 'username', message: 'this is the description', star_rate: 5});	
-});
+
     // <h1><%= username %></h1>
     // <p><%= message %></p>
     //, useranme: 'this is a user name', message: 'hello'
 // app.get('/hello', function(req, res) {
 // 	res.render('hello', {message: 'a'});	
 // });
+app.get('/hello/:id', function(req, res) {
+
+    //res.render('mytest', {customized_name: 'lopop page', username: 'username', message: 'this is the description', star_rate: 5});
+
+    var Pop = Parse.Object.extend("Pop");
+    //res.send('1');
+    var query = new Parse.Query(Pop);
+    query.get(req.params.id, {
+        success: function(pop) {
+
+            var description = pop.get("popDescription");
+            var userId = pop.get("seller");
+            //res.send(userId.id);
+            var User = Parse.Object.extend("User");
+            var userQuery = new Parse.Query(User);
+            var item_picz = pop.get("images");
+            //res.send(item_pic[0]["_url"]);
+            //res.send(description);
+            userQuery.get(userId.id, {
+                success: function(user) {
+                    //
+                    var username = user.get("name");
+                    var pic = user.get("profilePictureUrl");
+                    //res.send(pic);
+
+                    //res.render('mytest', {customized_name: 'lopop page', username: username, message: description, star_rate: 5, profile_pic: pic, item_pic: item_picz[0]["_url"]});
+                    res.render('test', {customized_name: 'lopop page', username: username, message: description, star_rate: 5, profile_pic: pic, item_pic: item_picz[0]["_url"]});
+                },
+                error: function(object, error) {
+                    res.send('user not found');
+                    res.end();
+                }
+            });
+
+        },
+        error: function(object, error) {
+
+           res.send('object not found');
+        }
+    });
+
+
+});
+app.get('/test', function(req, res) {
+    res.render('test');
+});
 
 // app.get('/hello', function(req, res) {
 // 	res.render('mytest', {supplies: 'a b c'});	
